846e57416a014fcaaea7928b93b699c837d9ed99
AlejoBerrio <alejo86-12@hotmail.com>
2021-09-19 18:50:14 -0500

    Delete PruebaOmniPro directory

--- PruebaOmniPro/src/main/java/co/com/linio/test/exceptions/InternetConnectionException.java
+++
@@ -1,9 +0,0 @@
-package co.com.linio.test.exceptions;
-
-public class InternetConnectionException extends RuntimeException {
-    public static final String INTERNET_FAILED = "Por favor verifique su conexiòn a Internet";
-
-    public InternetConnectionException(String message) {
-        super(message);
-    }
-}
